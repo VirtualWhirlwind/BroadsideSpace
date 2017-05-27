9224c3e40fd458236e0311494d8773c682919a5c
kennebel <jac@kennebel.com>
2017-05-27 08:58:57 -0600

    Prep work

---
+++ Assets/B_Scripts/1_Global/GlobalManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalManager : MonoBehaviour
+{
+    #region Unity Events
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/B_Scripts/3_Game/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    #region Unity Events
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+}
